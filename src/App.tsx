1a8792c401bb19357de59874124e65d8a5a0b534
Hop Nguyen <nguyenthehop2000@gmail.com>
2025-01-06 10:00:16 +0000

    Try to fix GH build page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import React from "react";
+// import './App.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (

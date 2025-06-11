0c6003ed06919ba233132c6fc700349be00c377c
The Hope <thehopewithoute@gmail.com>
2025-06-11 23:49:50 +0700

    Update GitHub Pages deployments config

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thehope2k.github.io',
-  base: '/portfolio',
-  outDir: './dist',
-  build: {
-    assets: 'assets'
-  }
+  base: '/portfolio/',
+  output: 'static'
 });

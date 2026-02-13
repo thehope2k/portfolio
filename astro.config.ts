13980ef4783b156571d02fc96ecf01785cc8c7e9
thehope <nguyenthehop2000@gmail.com>
2026-02-13 09:53:45 +0700

    Update site domain

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thehope2k.github.io',
-  base: '/portfolio/',
+  site: 'https://thehope2k.com',
+  base: '/',
   output: 'static'
 });
